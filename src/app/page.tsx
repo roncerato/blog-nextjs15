289b3ac5b849c78baf224e1cb68b0eaff7ea391e
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-17 17:00:17 +0500

    Added login link in component Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>
         Home Page
       </h1>
+      <div>
+        <Link href="/api/auth/login"> Login</Link>
+      </div>
     </div>
   );
 }
