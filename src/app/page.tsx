97b25b94ed1642a3949189c43d96b1028eba8155
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-17 17:44:16 +0500

    Switched Auth0 on new version and configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import Link from "next/link";
+import { auth0 } from "@/lib/auth0"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth0.getSession()
+
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">Sign up</a>
+        <a href="/auth/login">Log in</a>
+      </main>
+    )
+  }
+
   return (
-    <div>
-      <h1>
-        Home Page
-      </h1>
-      <div>
-        <Link href="/api/auth/login"> Login</Link>
-      </div>
-    </div>
-  );
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+      <a href="/auth/logout">Log out</a>
+    </main>
+  )
 }
