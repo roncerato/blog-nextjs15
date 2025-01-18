75a992b7c0eb47099caaaa75b573db62ecd22ca4
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-01-19 01:30:55 +0500

    Added a hero image and a Logo component on the main page with updated content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image"
+import HeroImage from "../../public/hero.webp"
+import { Logo } from "@/components/Logo"
+
 export default async function Home() {
     return (
-      <main>
-        <a href="/auth/login?screen_hint=signup">Sign up</a>
-        <a href="/auth/login">Log in</a>
+      <main className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+       <Image src={HeroImage} alt="Hero" fill className="absolute"/>
+       <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm" >
+        <Logo/>
+        <p>
+          The AI-powered SAAS solution to generate SEO-optimized blog posts in minutes. Get high-quality content, without sacrificing your time.
+        </p>
+        <a href="/auth/login" className="btn my-4">
+        Begin
+        </a>
+       </div>
       </main>
     )
 }
