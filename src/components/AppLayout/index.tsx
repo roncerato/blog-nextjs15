cc274ca1c4b29bd250feffa85672865a42b5e27b
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-01-18 20:28:37 +0500

    Implemented the sidebar and design style using Tailwind CSS

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -9,18 +9,18 @@ export const AppLayout = async ({ children }: AppLayoutProps) => {
         <main className="grid grid-cols-[300px,1fr] h-screen max-h-screen">
             {
                 !isRoot &&
-                <aside className="flex flex-col text-white overflow-hidden bg-slate-800">
-                    <div>
+                <aside className="flex flex-col text-white overflow-hidden">
+                    <header className="bg-slate-800">
                         <div>Logo</div>
                         <div>cta button</div>
                         <div>tokens</div>
-                    </div>
-                    <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+                    </header>
+                    <main className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
                         list of post
-                    </div>
-                    <div className="bg-cyan-800">
+                    </main>
+                    <footer className="bg-cyan-800">
                         user information - logout button
-                    </div>
+                    </footer>
                 </aside>
             }
             <div >
