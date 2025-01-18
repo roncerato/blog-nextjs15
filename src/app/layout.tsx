e2b42cf0fbf2bf411680ca754e539ee4d34c76a9
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-01-19 01:28:28 +0500

    DM Sans and DM Serif Display fonts have been added, and the body class has been updated to use new fonts in the application layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import { AppLayout } from "../components/AppLayout";
 import "./globals.scss";
+import { DM_Sans, DM_Serif_Display } from "next/font/google";
 
+const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: "--font-dm-sans",
+});
+const dmSerifDisplay = DM_Serif_Display({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: "--font-dm-serif",
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${dmSans.variable} ${dmSerifDisplay.variable} font-body`}>
         <AppLayout>
           {children}
         </AppLayout>
