e4db243fdacb977171f89df2343ed28482b634d0
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-01-19 18:46:34 +0500

    Added a click handler for adding tokens via a POST request

--- src/app/token-topup/page.tsx
+++ src/app/token-topup/page.tsx
@@ -1,9 +1,24 @@
+"use client"
 export default function TokenTopup() {
+    const handleClick = async () => {
+        const response = await fetch('/api/addTokens', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 100 })
+        })
+        const json = await response.json()
+       console.log(json)
+    }
     return (
         <div>
             <h1>
                 Token-Topup Page
             </h1>
+            <button className="btn" onClick={handleClick}>
+                Add tokens
+            </button>
         </div>
     );
 }
