9ac6e70598dcc82755c639ae88ca7083e3de4bcd
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-01-19 14:16:07 +0500

    A new functional design of the post creation page has been added: a form for entering topics and keywords, processing data transmission and displaying the generated post.

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -1,21 +1,54 @@
 "use client"
+
+import { useState } from "react"
+
 export default function NewPost() {
-    const handleClick = async () => {
+    const [postContent, setPostContent] = useState<string | JSON | null>(null)
+    const [topic, setTopic] = useState<string>("Top 10 tips for dog owners")
+    const [keywords, setKeywords] = useState<string>("first-time dog owners, common dog health issues, best dog breeds")
+
+    const handleSubmit = async (e: React.MouseEvent<HTMLFormElement>) => {
+        e.preventDefault()
         const response = await fetch('/api/generatePost', {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ topic, keywords })
         })
-        const data = await response.json()
+        const json = await response.json()
         alert("Post generated successfully")
-        console.log(data)
+        // setPostContent(JSON.stringify(json))
+        // console.log(json)
+        const formatedJSON = json.slice(json.indexOf("{"), json.indexOf("}") + 1)
+        console.log(formatedJSON)
+        setPostContent(JSON.parse(formatedJSON))
     }
     return (
         <div>
-            <h1>
-                New Post Page
-            </h1>
-            <button onClick={handleClick} className="btn">
-                Generate Post
-            </button>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        <strong>
+                        Generate a blog post on the topic of:
+                        </strong>
+                    </label>
+                    <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={e => setTopic(e.target.value)}/>                    
+                </div>
+                <div>
+                    <label>
+                        <strong>
+                        Targeting the following keywords:
+                        </strong>
+                    </label>
+                    <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={e => setKeywords(e.target.value)}/>                    
+                </div>
+                
+                <button type="submit" className="btn">
+                    Generate Post
+                </button>
+            </form>
+            <div className="max-w-screen-sm p-10" dangerouslySetInnerHTML={{ __html: JSON.stringify(postContent!) }} />
         </div>
     );
 }
