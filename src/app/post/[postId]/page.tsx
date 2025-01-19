00ba70d1cb76bf2a23300ae33efe7026becec2be
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-01-19 21:35:55 +0500

    Changed the type of the postId parameter to Promise for asynchronous extraction, improved getting a post from the database

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -4,7 +4,8 @@ import { ObjectId } from "mongodb";
 import { redirect } from "next/navigation";
 import { IDBUser, IDBPosts } from "@/types/db";
 
-export default async function Post({ params }: { params: { postId: string } }) {
+export default async function Post({ params }: {params: Promise<{ postId: string }>}) {
+    const { postId } = await params
     const userSession = await auth0.getSession();
     const client = await clientPromise;
     const db = client.db("BlogStandart")
@@ -12,7 +13,7 @@ export default async function Post({ params }: { params: { postId: string } }) {
         auth0Id: userSession?.user.sub
     })
     const post = await db.collection<IDBPosts>("posts").findOne({
-        _id: new ObjectId(params.postId),
+        _id: new ObjectId(postId),
         userId: user?._id
     })
     if (!post) {
