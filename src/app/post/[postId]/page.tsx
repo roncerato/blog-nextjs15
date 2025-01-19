4ab6a4926ff73c6034c2b6786703659470778989
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-01-19 21:58:15 +0500

    Added the display of the title, meta description and keywords in the post page, improved the visual interface and component structure.

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -3,6 +3,8 @@ import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { redirect } from "next/navigation";
 import { IDBUser, IDBPosts } from "@/types/db";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 
 export default async function Post({ params }: {params: Promise<{ postId: string }>}) {
     const { postId } = await params
@@ -20,10 +22,33 @@ export default async function Post({ params }: {params: Promise<{ postId: string
         redirect("/post/new");
       }
     return (
-        <div>
-            <h1>
-                Post Page
-            </h1>
+        <div className="overflow-auto h-full">
+        <div className="max-w-screen-sm mx-auto">
+  
+          <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
+            SEO title & meta description
+          </div>
+  
+          <div className="p-4 my-2 border border-stone-200 rounded-md">
+            <div className="text-blue-600 text-2xl font-bold">{post.title}</div>
+            <div className="mt-2">{post.metaDescription}</div>
+          </div>
+          <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
+            Keywords
+          </div>
+          <div className="flex flex-wrap pt-2 gap-1">
+            {post.keywords.split(",").map((keyword, i) => (
+              <div key={i} className="p-2 rounded-full bg-slate-800 text-white flex">
+                <FontAwesomeIcon icon={faHashtag} width={10}/> {keyword}
+              </div>
+            ))}
+          </div>
+          <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
+            Blog post
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: post.postContent || "" }} />
+          
         </div>
+      </div>
     );
 }
