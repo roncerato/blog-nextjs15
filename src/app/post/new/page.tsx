b1a43f8e026d9cca1ea5ef3b790d3c2de61fb834
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-21 13:09:21 +0500

    The display of the post generation indicator has been added and the interface of the form with suggestions for entering keywords has been improved.

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -1,54 +1,75 @@
 "use client"
 
-import { useRouter } from "next/navigation" 
+import { faBrain } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function NewPost() {
     const [topic, setTopic] = useState<string>("Top 10 tips for dog owners")
     const [keywords, setKeywords] = useState<string>("first-time dog owners, common dog health issues, best dog breeds")
     const router = useRouter();
-
+    const [generating, setGenerating] = useState(false)
     const handleSubmit = async (e: React.MouseEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const response = await fetch('/api/generatePost', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ topic, keywords })
-        })
-        const json = await response.json()
-        alert("Post generated successfully")
+        setGenerating(true)
+        try {
 
-        console.log(json)
-        if(json.postId){
-            router.push(`/post/${json.postId}`)
+            const response = await fetch(`/api/generatePost`, {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({ topic, keywords })
+            })
+            const json = await response.json()
+            if (json?.postId) {
+                router.push(`/post/${json.postId}`)
+            }
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (error) {
+            setGenerating(false)
         }
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>
-                        <strong>
-                            Generate a blog post on the topic of:
-                        </strong>
-                    </label>
-                    <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={e => setTopic(e.target.value)} />
+        <div className="h-full overflow-hidden">
+            {
+                !!generating && (<div className="text-green-500 flex h-full animate-pulse w-full  items-center justify-center flex-col">
+                    <FontAwesomeIcon icon={faBrain} className="text-8xl" />
+                    <h6>Generating...</h6>
+                </div>)
+            }
+            {!generating && (
+                <div className="w-full h-full flex flex-col overflow-auto">
+                    <form onSubmit={handleSubmit} className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200">
+                        <div>
+                            <label>
+                                <strong>
+                                    Generate a blog post on the topic of:
+                                </strong>
+                            </label>
+                            <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={e => setTopic(e.target.value)} maxLength={80} />
+                        </div>
+                        <div>
+                            <label>
+                                <strong>
+                                    Targeting the following keywords:
+                                </strong>
+                            </label>
+                            <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={e => setKeywords(e.target.value)} maxLength={80} />
+                            <small className="block mb-2 ">
+                                Separate keywords with commas. <br />
+                                <i>
+                                    For example: &quot;blogging, writing, content marketing&quot;
+                                </i>
+                            </small>
+                        </div>
+                        <button className="btn" type="submit" disabled={!topic.trim() || !keywords.trim()}>
+                            Generate
+                        </button>
+                    </form>
                 </div>
-                <div>
-                    <label>
-                        <strong>
-                            Targeting the following keywords:
-                        </strong>
-                    </label>
-                    <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={e => setKeywords(e.target.value)} />
-                </div>
-
-                <button type="submit" className="btn">
-                    Generate Post
-                </button>
-            </form>
+            )}
         </div>
     );
 }
