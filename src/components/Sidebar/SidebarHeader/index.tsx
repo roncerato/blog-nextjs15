337973ff725b2565e67038435ae003cf701c8cb8
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-22 11:31:06 +0500

    Moved the SidebarHeader and SidebarFooter components to the Sidebar folder for a better project structure

--- src/components/Sidebar/SidebarHeader/index.tsx
+++ src/components/Sidebar/SidebarHeader/index.tsx
@@ -2,17 +2,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { SidebarHeaderProps } from "./SidebarHeader.props"
 import Link from "next/link"
 import { faCoins } from "@fortawesome/free-solid-svg-icons"
-import { Logo } from "../Logo"
+import { Logo } from "@/components/Logo"
 export const SidebarHeader = async ({ availableTokens }: SidebarHeaderProps) => {
     return (
         <header className="bg-slate-800 px-2">
-            <Logo/>
+            <Logo />
             <Link href="/post/new" className="btn">
                 New post
             </Link>
             <div>
                 <Link href="/token-topup" className="flex mt-2 justify-center items-center">
-                    <FontAwesomeIcon icon={faCoins} className="text-yellow-500" width={20}/>
+                    <FontAwesomeIcon icon={faCoins} className="text-yellow-500" width={20} />
                     <span className="pl-1">
                         {availableTokens} tokens available
                     </span>
