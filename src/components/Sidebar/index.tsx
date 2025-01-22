fc03fb777258a82c08e64aefd8c0fa813a34388f
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-22 14:13:13 +0500

    Added the ISidebarProps interface and simplified the Sidebar component by removing asynchronous calls and passing empty passes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,39 +1,14 @@
-import { auth0 } from "@/lib/auth0";
-import clientPromise from "@/lib/mongodb";
-import { IDBUser, IDBPosts } from "@/types/db";
-import { headers } from "next/headers";
+import { ISidebarProps } from "./Sidebar.props";
 import { SidebarFooter } from "./SidebarFooter";
 import { SidebarHeader } from "./SidebarHeader";
 import SidebarMain from "./SidebarMain";
 
-export default async function Sidebar() {
-
-    const headerList = await headers()
-    const path = headerList.get("x-current-path")
-    const isRoot = path === "/"
-    const userSession = await auth0.getSession();
-    const client = await clientPromise;
-    const db = client.db("BlogStandart")
-    const user = await db.collection<IDBUser>("users").findOne({
-        auth0Id: userSession?.user.sub
-    })
-
-    const posts = await db.collection<IDBPosts>("posts")
-        .find({ userId: user?._id })
-        .sort({ created: -1 })
-        .limit(5)
-        .toArray()
-
+export default function Sidebar({ }: ISidebarProps) {
     return (
-        <>
-            {
-                !isRoot &&
-                <aside className="flex flex-col text-white overflow-hidden">
-                    <SidebarHeader availableTokens={user?.availableTokens ?? 0} />
-                    <SidebarMain posts={posts} />
-                    <SidebarFooter />
-                </aside>
-            }
-        </>
+        <aside className="flex flex-col text-white overflow-hidden">
+            <SidebarHeader />
+            <SidebarMain />
+            <SidebarFooter />
+        </aside>
     )
 }
