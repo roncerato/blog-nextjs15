a0b1c17cc23c74d038e9136e0b51356bced00646
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-24 16:02:47 +0500

    Replace <a> with <Link> for client-side navigation

--- src/components/Sidebar/SidebarMain/index.tsx
+++ src/components/Sidebar/SidebarMain/index.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import { ISidebarMainProps } from "./SidebarMain.props";
 import { usePostsContext } from "@/context/PostContext";
+import Link from "next/link";
 
 export default function SidebarMain({ posts: postsFromSSR }: ISidebarMainProps) {
 
@@ -14,18 +15,18 @@ export default function SidebarMain({ posts: postsFromSSR }: ISidebarMainProps) 
     return (
         <main className="px-2 flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800 scrollbar-custom">
             {posts.map(post => (
-                <a
+                <Link
                     className={`py-1 border border-white/0 block text-ellipsis overflow-hidden whitespace-nowrap my-1 px-2 bg-white/10 cursor-pointer rounded-sm`}
                     key={String(post._id)}
                     href={`/post/${post._id}`}>
                     {post.topic}
-                </a>
+                </Link>
             ))}
-            <button 
-            className="block hover:underline text-sm text-slate-400 text-center cursor-pointer mx-auto mt-4"
-            onClick={()=>{
-                getPosts({lastPostDate: posts[posts.length - 1].createdAt})
-            }}>
+            <button
+                className="block hover:underline text-sm text-slate-400 text-center cursor-pointer mx-auto mt-4"
+                onClick={() => {
+                    getPosts({ lastPostDate: posts[posts.length - 1].createdAt })
+                }}>
                 Load more posts
             </button>
         </main>
