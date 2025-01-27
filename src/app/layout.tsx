5541365ba0887517b709b67e8599fc4e6ed504b7
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-27 13:52:02 +0500

    Renamed the PostContext file to PostsContext and changed import this file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.scss";
 import { DM_Sans, DM_Serif_Display } from "next/font/google";
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core";
-import { PostsProvider } from "@/context/PostContext";
+import { PostsProvider } from "@/context/PostsContext";
 config.autoAddCss = false;
 const dmSans = DM_Sans({
   weight: ['400', '500', '700'],
--- src/components/Sidebar/SidebarMain/index.tsx
+++ src/components/Sidebar/SidebarMain/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useEffect } from "react";
 import { ISidebarMainProps } from "./SidebarMain.props";
-import { usePostsContext } from "@/context/PostContext";
+import { usePostsContext } from "@/context/PostsContext";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
