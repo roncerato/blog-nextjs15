f5b0340a04065bc4c767b61af5f0611d1822fe0d
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-28 14:58:10 +0500

    Added the functionality of deleting posts in the Sidebar Main component using the button and icon

--- src/components/Sidebar/SidebarMain/index.tsx
+++ src/components/Sidebar/SidebarMain/index.tsx
@@ -1,7 +1,10 @@
 "use client"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ISidebarMainProps } from "./SidebarMain.props";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { faRemove } from "@fortawesome/free-solid-svg-icons";
+import { ObjectId } from "mongodb";
 
 export default function SidebarMain({ posts }: ISidebarMainProps) {
 
@@ -9,16 +12,37 @@ export default function SidebarMain({ posts }: ISidebarMainProps) {
 
     const postId = pathname?.match(/^\/post\/(?!new$)([a-zA-Z0-9]+)/)?.[1] || null;
 
+    const handleDelete = async (postId: string | ObjectId) => {
+        const res = await fetch(`/api/deletePost`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ postId })
+        });
+        const json = await res.json()
+        console.log(json)
+    }
     return (
         <main className="px-2 flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800 scrollbar-custom">
-            {posts.map(post => (
-                <Link
-                    className={`py-1 border border-white/0 block text-ellipsis overflow-hidden whitespace-nowrap my-1 px-2 bg-white/10 cursor-pointer rounded-sm ${postId === post._id ? "bg-white/20 border-white" : ""}`}
-                    key={String(post._id)}
-                    href={`/post/${post._id}`}>
-                    {post.topic}
-                </Link>
-            ))}
+            <ul>
+
+                {posts.map(post => (
+                    <li
+                        key={String(post._id)}
+                        className={`py-1 border border-white/0 flex justify-between gap-2 my-1 px-2 bg-white/10 cursor-pointer rounded-sm ${postId === post._id ? "bg-white/20 border-white" : ""}`}
+                    >
+                        <Link
+                            className="text-ellipsis overflow-hidden whitespace-nowrap"
+                            href={`/post/${post._id}`}>
+                            {post.topic}
+                        </Link>
+                        <button>
+                            <FontAwesomeIcon icon={faRemove} onClick={() => { handleDelete(post._id) }} />
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
