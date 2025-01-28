f189443fe6ac5eeffdeea65875488e395fae19ec
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-28 11:19:15 +0500

    Replaced the rendering of the post with the PostClient component with the transformation of the post data.

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -3,8 +3,7 @@ import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { redirect } from "next/navigation";
 import { IDBUser, IDBPost } from "@/types/db";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import PostClient from "@/components/PostClient";
 
 export default async function Post({ params }: { params: Promise<{ postId: string }> }) {
   const { postId } = await params
@@ -21,34 +20,14 @@ export default async function Post({ params }: { params: Promise<{ postId: strin
   if (!post) {
     redirect("/post/new");
   }
+  const postConverted = {
+    ...post,
+    _id: post._id.toString(),
+    userId: post.userId.toString(),
+  }
   return (
     <div className="overflow-auto h-full post-body">
-      <div className="max-w-screen-sm mx-auto">
-
-        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
-          SEO title & meta description
-        </div>
-
-        <div className="p-4 my-2 border border-stone-200 rounded-md">
-          <div className="text-blue-600 text-2xl font-bold">{post.title}</div>
-          <div className="mt-2">{post.metaDescription}</div>
-        </div>
-        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
-          Keywords
-        </div>
-        <div className="flex flex-wrap pt-2 gap-1">
-          {post.keywords.split(",").map((keyword, i) => (
-            <div key={i} className="p-2 rounded-full bg-slate-800 text-white flex">
-              <FontAwesomeIcon icon={faHashtag} width={10} /> {keyword}
-            </div>
-          ))}
-        </div>
-        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
-          Blog post
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: post.postContent || "" }} />
-
-      </div>
+      <PostClient post={postConverted} />
     </div>
   );
 }
