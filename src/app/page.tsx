8006e286da60e66378185d04e8580f220aaaeabb
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-02-04 13:09:27 +0500

    Added metadata for pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image"
 import HeroImage from "../../public/hero.webp"
 import { Logo } from "@/components/Logo"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Home"
+}
 
 export default async function Home() {
-    return (
-      <main className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
-       <Image src={HeroImage} alt="Hero" fill className="absolute"/>
-       <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm" >
-        <Logo/>
+  return (
+    <main className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={HeroImage} alt="Hero" fill className="absolute" />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm" >
+        <Logo />
         <p>
           The AI-powered SAAS solution to generate SEO-optimized blog posts in minutes. Get high-quality content, without sacrificing your time.
         </p>
         <a href="/auth/login" className="btn my-4">
-        Begin
+          Begin
         </a>
-       </div>
-      </main>
-    )
+      </div>
+    </main>
+  )
 }
