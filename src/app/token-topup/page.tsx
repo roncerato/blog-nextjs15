118b4ce32ff35747d4faf9f8f2200db92742287e
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-02-21 13:50:33 +0500

    Сhanged the description of the page and added price cards for selecting token packages.

--- src/app/token-topup/page.tsx
+++ src/app/token-topup/page.tsx
@@ -1,24 +1,23 @@
 "use client"
+
+import PricingCard from "@/components/PricingCard"
+
 export default function TokenTopup() {
-    const handleClick = async () => {
-        const response = await fetch('/api/addTokens', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        const json = await response.json()
-        console.log(json)
-        window.location.href = json.session.url;
-    }
+
     return (
-        <div className="h-full flex items-center justify-center flex-col col-span-1 gap-8">
+        <div className="h-full overflow-x-hidden grid content-center justify-center gap-6">
             <h1 className="text-4xl font-bold text-center">
-                Token-Topup Page
+                Select the appropriate token package
             </h1>
-            <button className="btn w-auto" onClick={handleClick}>
+            <div className="flex flex-nowrap overflow-x-auto gap-4 pricing-cards-scrollbar p-4">
+
+                <PricingCard name={"Quick Draft"} price={25} tokens={250} desc={"Запускайте SEO-посты за минуты – идеальный старт для вашего блога."} />
+                <PricingCard name={"SEO Booster"} price={75} tokens={1000} desc={"Регулярное создание текстов для устойчивого роста сайта."} />
+                <PricingCard name={"Content Empire"} price={150} tokens={2200} desc={"Масштабируйте контент-маркетинг с расширенным пакетом возможностей."} />
+            </div>
+            {/* <button className="btn w-auto" onClick={handleClick}>
                 Add tokens
-            </button>
+            </button> */}
         </div>
     );
 }
