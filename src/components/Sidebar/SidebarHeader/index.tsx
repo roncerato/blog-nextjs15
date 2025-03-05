b8836dc7b44a6b4cdf6d1ef7ba8de72b88fae4ad
roncerato <nazarmuhamedow9731@gmail.com>
2025-03-05 23:33:10 +0500

    The sidebar title has been updated: the design has been improved and new icons have been added to create a post and replenish tokens.

--- src/components/Sidebar/SidebarHeader/index.tsx
+++ src/components/Sidebar/SidebarHeader/index.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { SidebarHeaderProps } from "./SidebarHeader.props"
 import Link from "next/link"
-import { faCoins } from "@fortawesome/free-solid-svg-icons"
-import { Logo } from "@/components/Logo"
+// import { faCoins } from "@fortawesome/free-solid-svg-icons"
+import Logo from "@/assets/logo.svg"
 import { useDataContext } from "@/context/DataContext";
 import { useEffect } from "react";
-
+import HideSidebar from "@/assets/hideSidebar.svg"
+import Plus from "@/assets/plus.svg"
+import Token from "@/assets/token.svg"
 export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) => {
 
     const { availableTokens, setAvailableTokens } = useDataContext()
@@ -15,20 +17,26 @@ export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) =
     }, [setAvailableTokens, tokens])
 
     return (
-        <header className="bg-slate-800 px-2">
-            <Logo />
-            <Link
-                href={!!availableTokens ? "/post/new" : "#"}
-                className={`btn ${!!availableTokens ? "" : "pointer-events-none opacity-50"}`}
-                aria-disabled={!!availableTokens}
-            >
-                New post
-            </Link>
-            <div>
-                <Link href="/token-topup" className="flex mt-2 justify-center items-center">
-                    <FontAwesomeIcon icon={faCoins} className="text-yellow-500" width={20} />
-                    <span className="pl-1">
-                        {availableTokens ? availableTokens : 0} tokens available
+        <header className="px-4">
+            <div className="pt-4 flex justify-between">
+                <HideSidebar fill="#ADADAE" height={24} width={24} className="hover:fill-[#6e6e6e]" />
+
+                <Link
+                    href={!!availableTokens ? "/post/new" : "#"}
+                    className={`flex justify-center items-center ${!!availableTokens ? "cursor-pointer" : "cursor-not-allowed"}`}
+                    aria-disabled={!!availableTokens}
+                    aria-label="Create a new post"
+                    title="Create a new post"
+                >
+                    <Plus fill="#ADADAE" height={18} width={18} className="hover:fill-[#6e6e6e]" />
+                </Link>
+            </div>
+            <div className="flex justify-between items-center mt-6 mb-4">
+                <Logo />
+                <Link href="/token-topup" className="flex justify-center items-center" title="Top up your tokens">
+                    <Token width={20} height={20} />
+                    <span className="text-black">
+                        {availableTokens ? availableTokens : 0}
                     </span>
                 </Link>
 
