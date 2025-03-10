fbb66237ca53655cfbdc5607f4b609ead10b1ef5
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-03-10 14:59:00 +0500

    Moved all icons from the src/assets folder to the public/svg folder and updated the import paths in the Sidebar components.

--- src/components/Sidebar/SidebarHeader/index.tsx
+++ src/components/Sidebar/SidebarHeader/index.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { SidebarHeaderProps } from "./SidebarHeader.props"
 import Link from "next/link"
-import Logo from "@/assets/logo.svg"
+import Logo from "public/svg/logo.svg"
 import { useDataContext } from "@/context/DataContext";
 import { useEffect } from "react";
-// import HideSidebar from "@/assets/hideSidebar.svg"
-// import Plus from "@/assets/plus.svg"
-// import Token from "@/assets/token.svg"
+import HideSidebar from "public/svg/hideSidebar.svg"
+import Plus from "public/svg/plus.svg"
+import Token from "public/svg/token.svg"
 export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) => {
 
     const { availableTokens, setAvailableTokens } = useDataContext()
@@ -17,7 +17,7 @@ export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) =
     return (
         <header className="px-4">
             <div className="pt-4 flex justify-between">
-                {/* <HideSidebar fill="#ADADAE" height={24} width={24} className="hover:fill-[#6e6e6e]" /> */}+
+                <HideSidebar fill="#ADADAE" height={24} width={24} className="hover:fill-[#6e6e6e]" />
 
                 <Link
                     href={!!availableTokens ? "/post/new" : "#"}
@@ -26,13 +26,13 @@ export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) =
                     aria-label="Create a new post"
                     title="Create a new post"
                 >
-                    {/* <Plus fill="#ADADAE" height={18} width={18} className="hover:fill-[#6e6e6e]" /> */}+
+                    <Plus fill="#ADADAE" height={18} width={18} className="hover:fill-[#6e6e6e]" />
                 </Link>
             </div>
             <div className="flex justify-between items-center mt-6 mb-4">
                 <Logo />
                 <Link href="/token-topup" className="flex justify-center gap-1 items-center" title="Top up your tokens">
-                    {/* <Token width={20} height={20} /> */}+
+                    <Token width={20} height={20} />
                     <h5 className="text-black font-semibold leading-1">
                         {availableTokens ? availableTokens : 0}
                     </h5>
