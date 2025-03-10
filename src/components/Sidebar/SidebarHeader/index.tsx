1b96d2ed1a3946ac31a8a0e33228033ae6bc666c
roncerato <nazarmuhamedow9731@gmail.com>
2025-03-10 22:01:37 +0500

    Added a logo to the SidebarHeader component

--- src/components/Sidebar/SidebarHeader/index.tsx
+++ src/components/Sidebar/SidebarHeader/index.tsx
@@ -5,9 +5,7 @@ import Link from "next/link"
 // import Logo from "@/assets/logo.svg"
 import { useDataContext } from "@/context/DataContext";
 import { useEffect } from "react";
-// import HideSidebar from "@/assets/hideSidebar.svg"
-// import Plus from "@/assets/plus.svg"
-// import Token from "@/assets/token.svg"
+import { Icons } from "@/assets/Icons";
 export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) => {
 
     const { availableTokens, setAvailableTokens } = useDataContext()
@@ -18,7 +16,7 @@ export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) =
     return (
         <header className="px-4">
             <div className="pt-4 flex justify-between">
-                {/* <HideSidebar fill="#ADADAE" height={24} width={24} className="hover:fill-[#6e6e6e]" /> */}+
+                <Icons.HideSidebar fill="#ADADAE" height={24} width={24} className="hover:fill-[#6e6e6e]" />
 
                 <Link
                     href={!!availableTokens ? "/post/new" : "#"}
@@ -27,13 +25,13 @@ export const SidebarHeader = ({ availableTokens: tokens }: SidebarHeaderProps) =
                     aria-label="Create a new post"
                     title="Create a new post"
                 >
-                    {/* <Plus fill="#ADADAE" height={18} width={18} className="hover:fill-[#6e6e6e]" /> */}+
+                    <Icons.Plus fill="#ADADAE" height={18} width={18} className="hover:fill-[#6e6e6e]" />
                 </Link>
             </div>
             <div className="flex justify-between items-center mt-6 mb-4">
-                {/* <Logo /> */}+
+                <Icons.Logo /> 
                 <Link href="/token-topup" className="flex justify-center gap-1 items-center" title="Top up your tokens">
-                    {/* <Token width={20} height={20} /> */}+
+                    <Icons.Token width={20} height={20} />
                     <h5 className="text-black font-semibold leading-1">
                         {availableTokens ? availableTokens : 0}
                     </h5>
