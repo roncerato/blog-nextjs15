aebb9fbc3becb57b1d7ea173604d91a6769a53bf
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-03-15 12:37:29 +0500

    The Sidebar component has been changed to receive user data via the API, including profile and posts, with an update of the status of available tokens.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,15 +1,41 @@
+
+"use client"
+import { useState, useEffect } from "react";
 import { ISidebarProps } from "./Sidebar.props";
 import { SidebarFooter } from "./SidebarFooter";
 import { SidebarHeader } from "./SidebarHeader";
 import SidebarMain from "./SidebarMain";
-import { getUserData } from "@/services/getUserData";
 import DataProvider from "@/context/DataContext";
-export default async function Sidebar({ }: ISidebarProps) {
+import { IDBPost, IDBUser } from "@/types/db";
+import { WithId } from "mongodb";
 
-    const { posts, availableTokens } = await getUserData()
+interface IUserData {
+    profile: WithId<IDBUser> | null;
+    posts: WithId<IDBPost>[];
+}
+
+export default function Sidebar({ }: ISidebarProps) {
+
+    const [posts, setPosts] = useState<WithId<IDBPost>[] | []>([]);
+    const [availableTokens, setAvailableTokens] = useState<number | undefined>(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('/api/getUserData', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+            const data = await res.json() as IUserData
+            setPosts(data.posts)
+            setAvailableTokens(data.profile?.availableTokens)
+        }
+        fetchData()
+    }, []);
 
     return (
-        <aside className="flex flex-col bg-[#F7F7F7] border-r-[1px] border-[#e5e7eb]">
+        <aside className="flex flex-col bg-[#F7F7F7] border-r-[1px] border-[#e5e7eb] max-h-full">
             <DataProvider>
                 <SidebarHeader availableTokens={availableTokens} />
                 <SidebarMain posts={posts} />
