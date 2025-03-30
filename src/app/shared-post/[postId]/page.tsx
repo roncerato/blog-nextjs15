694ceb1dae5f236ceb9f161c01df756673e2efab
Ron Cerato <nazarmuhamedow9731@gmail.com>
2025-03-30 16:38:04 +0500

    Added the display of the logo in the absence of a user session on the post page.

--- src/app/shared-post/[postId]/page.tsx
+++ src/app/shared-post/[postId]/page.tsx
@@ -1,4 +1,6 @@
+import { Icons } from "@/assets/Icons";
 import PostBody from "@/components/PostBody";
+import { auth0 } from "@/lib/auth0";
 import clientPromise from "@/lib/mongodb";
 import { IDBPost } from "@/types/db";
 import { ObjectId } from "mongodb";
@@ -6,6 +8,8 @@ import { redirect } from "next/navigation";
 
 
 export default async function SharedPost({ params }: { params: Promise<{ postId: string }> }) {
+  const session = await auth0.getSession()
+  const isSessionNull = session === null
   const { postId } = await params
   const client = await clientPromise;
   const db = client.db("BlogStandart")
@@ -18,6 +22,13 @@ export default async function SharedPost({ params }: { params: Promise<{ postId:
     redirect("/post/new");
   }
   return (
-    <PostBody post={post}/>
+    <>
+      {isSessionNull && <div className={`absolute top-0 left-0 p-4 grid w-full backdrop-blur z-40 bg-white/65`}>
+        <div className={`col-start-1 col-end-2 row-start-1 row-end-2 justify-self-center `}>
+          <Icons.Logo className="w-20" />
+        </div>
+      </div>}
+      <PostBody post={post} />
+    </>
   );
 }
