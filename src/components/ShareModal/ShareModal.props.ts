812d20cf3d30e596bb5bda4c632cb3edb092c068
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-04-29 15:50:21 +0500

    The DeleteModal and ShareModal components, along with their properties, have been removed

--- src/components/ShareModal/ShareModal.props.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectId } from "mongodb";
-import { Dispatch, SetStateAction } from "react";
-
-export interface ShareModalProps { 
-    id?: string | ObjectId, 
-    setIsModalOpened: Dispatch<SetStateAction<false | "share" | "delete">> }
